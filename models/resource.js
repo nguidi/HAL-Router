e951130d2058cb9d04b955ddfae521c81a961f60
nguidi <neri3004@gmail.com>
2013-07-24 18:57:06 -0300

    Inheritance

--- models/resource.js
+++ models/resource.js
@@ -10,13 +10,15 @@ module.exports
 		=	require('../lib/checkout.js')(app)
 		,	checkout_result
 		=	checkout()
+		,	transforms
+		=	app.get('transforms')
 
 		if	(checkout_result.errors!=0)
 			throw	new Error("Correct Errors to Continue")
 
 		app.set('Model',{})
 		_.each(
-			app.get('transforms')
+			transforms
 		,	function(transform,transform_name)
 			{
 				app.get('Model')[transform_name]
